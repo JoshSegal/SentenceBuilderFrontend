8a5fdf70173bb8f4fbe923053e07b6daa213ff23
Joshua Segal <joshuasegal4@gmail.com>
2021-12-26 22:25:01 +0200

    Endpoint name change

--- src/app/services/words.service.ts
+++ src/app/services/words.service.ts
@@ -12,37 +12,7 @@ export class WordsService {
 
 
 getWords() {
-  return this.http.get(`${this.uri}/nouns`);
-}
-
-getWordByNoun(noun: any) {
-  return this.http.get(`${this.uri}/words/${noun}`);
-}
-
-getWordByVerb(Verb: any) {
-  return this.http.get(`${this.uri}/words/${Verb}`);
-}
-getWordByAdjective(Adjective: any) {
-  return this.http.get(`${this.uri}/words/${Adjective}`);
-}
-getWordByPronoun(Pronoun: any) {
-  return this.http.get(`${this.uri}/words/${Pronoun}`);
-}
-
-
-addWords(Noun: string, Verb: string, Adjective: string, Adverb: string, Pronoun: string, Preposition: string, Conjunction: string, Determiner: string, Exclamation: string) {
-  const words = {
-    Noun :        Noun,
-    Verb :        Verb,
-    Adjective :   Adjective,
-    Adverb :      Adverb,
-    Pronoun :     Pronoun,
-    Preposition : Preposition,
-    Conjunction : Conjunction,
-    Determiner :  Determiner,
-    Exclamation : Exclamation
-  };
-  return this.http.post(`${this.uri}/words/add`, words);
+  return this.http.get(`${this.uri}/words`);
 }
 }
      
